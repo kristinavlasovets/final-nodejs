5abdd4879748582ec79c7b4da598029ac05af946
kristinavlasovets <61822704+kristinavlasovets@users.noreply.github.com>
2022-12-02 14:57:19 +0300

    added logout function

--- service/user-service.js
+++ service/user-service.js
@@ -39,6 +39,11 @@ class UserService {
 
     return { ...tokens, user: userDto };
   }
+
+  async logout(refreshToken) {
+    const token = await tokenService.removeToken(refreshToken);
+    return token;
+  }
 }
 
 module.exports = new UserService();
