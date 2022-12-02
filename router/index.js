01f1567cbb693e813bd1d0bea34361677a85ed93
kristinavlasovets <61822704+kristinavlasovets@users.noreply.github.com>
2022-12-02 14:28:03 +0300

    created login function

--- router/index.js
+++ router/index.js
@@ -1,11 +1,17 @@
-const Router = require('express').Router;
-const userController = require('../controllers/user-controller');
+const Router = require("express").Router;
+const userController = require("../controllers/user-controller");
 const router = new Router();
+const { body } = require("express-validator");
 
-router.post('/registration', userController.registration);
-router.post('/login', userController.login);
-router.post('/logout', userController.logout);
-router.post('/refresh', userController.refresh);
-router.post('/users', userController.getUsers);
+router.post(
+  "/registration",
+  body("email").isEmail(),
+  body("password").isLength({ min: 3, max: 29 }),
+  userController.registration
+);
+router.post("/login", userController.login);
+router.post("/logout", userController.logout);
+router.post("/refresh", userController.refresh);
+router.post("/users", userController.getUsers);
 
 module.exports = router;
--- service/user-service.js
+++ service/user-service.js
@@ -1,26 +1,44 @@
-const UserModel = require('../models/user-model');
-const bcrypt = require('bcrypt');
-const tokenService = require('./token-service');
-const UserDto = require('../dtos/user-dto');
+const UserModel = require("../models/user-model");
+const bcrypt = require("bcrypt");
+const tokenService = require("./token-service");
+const UserDto = require("../dtos/user-dto");
 
 class UserService {
-	async registration(email, password) {
-		const candidate = await UserModel.findOne({email});
-		if (candidate) {
-			throw new Error('Email address is already registered.');
-		}
-		const hashPassword = await bcrypt.hash(password, 3);
-		const user = await UserModel.create({
-			email,
-			password: hashPassword,
-		});
+  async registration(email, password) {
+    const candidate = await UserModel.findOne({ email });
+    if (candidate) {
+      throw new Error("Email address is already registered.");
+    }
+    const hashPassword = await bcrypt.hash(password, 3);
+    const user = await UserModel.create({
+      email,
+      password: hashPassword,
+    });
 
-		const userDto = new UserDto(user);
-		const tokens = tokenService.generateTokens({...userDto});
-		await tokenService.saveToken(userDto.id, tokens.refreshToken);
+    const userDto = new UserDto(user);
+    const tokens = tokenService.generateTokens({ ...userDto });
+    await tokenService.saveToken(userDto.id, tokens.refreshToken);
 
-		return {...tokens, user: userDto};
-	}
+    return { ...tokens, user: userDto };
+  }
+
+  async login(email, password) {
+    const user = await UserModel.findOne({ email });
+    if (!user) {
+      throw new Error(`Email address '${email}' is not registered.`);
+    }
+
+    const validPassword = await bcrypt.compare(password, user.password);
+    if (!validPassword) {
+      throw new Error("Incorrect password.");
+    }
+
+    const userDto = new UserDto(user);
+    const tokens = tokenService.generateTokens({ ...userDto });
+    await tokenService.saveToken(userDto.id, tokens.refreshToken);
+
+    return { ...tokens, user: userDto };
+  }
 }
 
 module.exports = new UserService();
