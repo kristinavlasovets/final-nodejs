31f5ef2aa0a4ed8b91569c976b5ba46eb7fe3e34
Kristina Vlasovets <kristinavlasovets@gmail.com>
2022-12-16 12:40:43 +0300

    added artpiece model

--- models/artPiece-model.js
+++ models/artPiece-model.js
@@ -3,7 +3,7 @@ const {Schema, model} = require('mongoose');
 const ArtPieceSchema = new Schema({
 	name: {type: String, required: true},
 	// stars: {type: Schema.Types.ObjectId, ref: 'Stars', default: 0},
-	stars: {type: Number, required: true},
+	stars: {type: Number, required: true, default: 0},
 });
 
 module.exports = model('ArtPiece', ArtPieceSchema);
